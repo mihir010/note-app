767dd6973a05eebb807759c47a54cf1dd1a29cf5
mihir010 <kumar.mihir02@gmail.com>
2024-03-09 02:33:52 +0530

    registered models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Note)
+admin.site.register(models.NotesRoom)
+admin.site.register(models.Tag)
+admin.site.register(models.TaggedItem)
+
+
+
